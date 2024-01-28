4d37477e7ee07007282e03dc7ad47eff57ca3739
Natasha Kanning <112439330+NatashaK88@users.noreply.github.com>
2024-01-27 18:31:55 -0700

    Add files via upload

---
+++ week 2/js/script.js
@@ -0,0 +1,12 @@
+//3 of your favorite websites
+console.log("amazon.com");
+console.log("facebook.com");
+console.log("tiktok.com");
+//3 of your favorite games if you play any - they can be board games, etc.
+console.log("Cribbage");
+console.log("Final Fantasy 7");
+console.log("Minecraft");
+//3 of your favorite artists and their cultural importance
+console.log("Amanda Palmer- Has had huge influence on mental health.");
+console.log("Catherine Opie- Has had a huge impact on the LGBTQIA community.");
+console.log("Jack London- is a social activist for things like workers rights.");
---
+++ week 2/readme.md
@@ -0,0 +1,3 @@
+The main differences between ChatGPT and my version are they put favorite websites in a console.log and I put that in a comment. Of course we had different favorites. The code however is exactly the same.
+
+I could explain the way chatGPT the code that ChatGPT used because we used the same basic code. I do not thing the work is mine because I did not create it. 
